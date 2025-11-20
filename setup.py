688ff99307cae4c6a0595579aaefdc4a42630552
AmmaraAkhtar <1144ammara@gmail.com>
2025-11-20 15:55:08 +0500

    basic setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+HYPHON_E_DOT="-e ."
+
+def get_requirements(filepath:str )-> List[str]:
+    requirements=[]
+    with open(filepath) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[i.replace('\n',"") for i in requirements]
+        if HYPHON_E_DOT in requirements:
+            requirements.remove(HYPHON_E_DOT)
+
+
+
+setup(name='ML Project',
+      version='1.0',
+      description='ML Project Pipeline',
+      author='Greg Ward',
+      author_email='ammaraakhtar93@gmail.com',
+      packages=find_packages(),
+      install_requires= get_requirements('requirements.txt')
+     )
